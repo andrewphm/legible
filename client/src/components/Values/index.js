d01009e6868fdfb06fc4c6989c4c4a5c0bb6af7e
andrewphm <andrpham@gmail.com>
2021-12-15 17:13:20 -0500

    Add Value image assets

--- client/src/components/Values/index.js
+++ client/src/components/Values/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
+// Images
+import img1 from '../../assets/values/accessibility.svg';
+import img2 from '../../assets/values/beauty.svg';
+import img3 from '../../assets/values/integrity.svg';
+import img4 from '../../assets/values/sustainability.svg';
+
 // Styled components
 import { Section } from './Values.styles';
 
