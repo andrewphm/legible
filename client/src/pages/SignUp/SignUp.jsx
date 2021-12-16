fa4423f998b458fa978ac0457340c99b35b180a2
andrewphm <andrpham@gmail.com>
2021-12-16 13:49:09 -0500

    Create sign up form

--- client/src/pages/SignUp/SignUp.jsx
+++ client/src/pages/SignUp/SignUp.jsx
@@ -1,7 +1,109 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { Visibility, VisibilityOff } from '@material-ui/icons';
+
+import {
+  Form,
+  FormHeading,
+  Input,
+  InputContainer,
+  InputWrapper,
+  Label,
+  Main,
+  StepHeading,
+} from './SignUp.styles';
+
+const initialForm = {
+  email: '',
+  password: '',
+  username: '',
+  displayName: '',
+};
+
+const StepOne = () => {
+  const [isPassVisible, setIsPassVisible] = useState(false);
+  const [isConfirmPassVisible, setIsConfirmPassVisible] = useState(false);
+
+  const handlePassVisibleClick = () => {
+    let ele = document.getElementById('password');
+    if (!isPassVisible) {
+      ele.type = 'text';
+    } else {
+      ele.type = 'password';
+    }
+    setIsPassVisible((prev) => !prev);
+  };
+  const handleConfirmPassVisibleClick = () => {
+    let ele = document.getElementById('confirm-password');
+    if (!isConfirmPassVisible) {
+      ele.type = 'text';
+    } else {
+      ele.type = 'password';
+    }
+    setIsConfirmPassVisible((prev) => !prev);
+  };
+
+  return (
+    <>
+      <StepHeading>SIGN UP TO LEGIBLE - STEP 1 OF 4</StepHeading>
+      <FormHeading>Create Account</FormHeading>
+      <Form>
+        <InputWrapper>
+          <InputContainer>
+            <Input
+              placeholder="E-mail"
+              type="text"
+              name="email"
+              autoFocus="true"
+            ></Input>
+            <Label htmlFor="email">Email</Label>
+          </InputContainer>
+        </InputWrapper>
+        <InputWrapper>
+          <InputContainer>
+            <Input
+              id="password"
+              placeholder="Password"
+              type="password"
+              name="password"
+            ></Input>
+            <Label htmlFor="password">Password</Label>
+            {isPassVisible ? (
+              <VisibilityOff onClick={handlePassVisibleClick} />
+            ) : (
+              <Visibility onClick={handlePassVisibleClick} />
+            )}
+          </InputContainer>
+        </InputWrapper>
+        <InputWrapper>
+          <InputContainer>
+            <Input
+              id="confirm-password"
+              placeholder="Confirm Password"
+              type="password"
+              name="password"
+            ></Input>
+            <Label htmlFor="password">Password</Label>
+            {isConfirmPassVisible ? (
+              <VisibilityOff onClick={handleConfirmPassVisibleClick} />
+            ) : (
+              <Visibility onClick={handleConfirmPassVisibleClick} />
+            )}
+          </InputContainer>
+        </InputWrapper>
+      </Form>
+    </>
+  );
+};
 
 const SignUp = () => {
-  return <div></div>;
+  const [form, setForm] = useState(initialForm);
+
+  return (
+    <Main>
+      <StepOne></StepOne>
+    </Main>
+  );
 };
 
 export default SignUp;
--- client/src/pages/SignUp/SignUp.styles.js
+++ client/src/pages/SignUp/SignUp.styles.js
@@ -2,23 +2,81 @@ import styled from 'styled-components';
 
 export const Main = styled.main`
   display: flex;
+  width: 100%;
+  height: 70vh;
+  padding: 45px 12px;
+  flex-direction: column;
+  align-items: center;
 `;
 
-export const StepHeading = styled.h1``;
+export const StepHeading = styled.h1`
+  text-align: center;
+  font-size: 0.9rem;
+  font-weight: 700;
+  letter-spacing: 0.12em;
+  width: 100%;
+  font-family: var(--primary-font);
+  color: rgb(95, 108, 116);
+`;
 
-export const FormHeading = styled.h2``;
-export const Form = styled.form``;
+export const FormHeading = styled.h2`
+  margin: 1rem auto;
+  font-size: 1.8rem;
+`;
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  padding: 10px;
+`;
 
 export const InputWrapper = styled.div`
-  margin-bottom: 2rem;
+  margin-bottom: 1.5rem;
   position: relative;
   width: 100%;
+  cursor: pointer;
 `;
 
+export const Label = styled.label`
+  position: absolute;
+  top: -10px;
+  background-color: white;
+  left: 10px;
+  font-weight: 600;
+  color: rgb(95, 108, 116);
+`;
 export const InputContainer = styled.div`
   position: relative;
+  width: 100%;
+  cursor: pointer;
+  border: 1px solid rgb(121, 146, 154);
+  border-radius: 0.2rem;
+  display: flex;
+  align-items: center;
+  padding-right: 10px;
 `;
 
-export const Input = styled.input``;
+export const Input = styled.input`
+  font-size: 1rem;
+  line-height: 1.35;
+  transition: (--transition);
+  background-color: #fff;
+  color: rgb(43, 50, 56);
+  display: flex;
+  width: 100%;
+  padding: 0.8rem;
+  cursor: pointer;
 
-export const Label = styled.label``;
+  :-webkit-autofill,
+  :-webkit-autofill:focus {
+    transition: background-color 600000s 0s, color 600000s 0s;
+  }
+  [data-autocompleted] {
+    background-color: transparent !important;
+  }
+
+  :focus {
+    outline: none !important;
+  }
+`;
