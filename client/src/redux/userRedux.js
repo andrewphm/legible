78e48046798e9b080fc59a3e4c2bb25664a74ce8
andrewphm <andrpham@gmail.com>
2021-12-17 14:28:45 -0500

    Add react-redux, redux toolkit, redux persist to dependency. Create User store and reducers. Create a persistent user state

---
+++ client/src/redux/userRedux.js
@@ -0,0 +1,32 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState: {
+    currentUser: null,
+    isFetching: false,
+    error: false,
+  },
+  reducers: {
+    loginStart: (state) => {
+      state.isFetching = true;
+    },
+    loginSuccess: (state, action) => {
+      state.error = false;
+      state.isFetching = false;
+      state.currentUser = action.payload;
+    },
+    loginFailure: (state) => {
+      state.isFetching = false;
+      state.error = true;
+    },
+    signout: (state) => {
+      state.currentUser = null;
+    },
+  },
+});
+
+export const { loginState, loginSucces, loginFailure, signout } =
+  userSlice.actions;
+
+export default userSlice.reducer;
