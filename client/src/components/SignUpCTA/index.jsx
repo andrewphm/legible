a19c6ca500ec66c6462791ae1db6c55be3a46712
andrewphm <andrpham@gmail.com>
2021-12-22 10:48:15 -0500

    Fix routing link and style

--- client/src/components/SignUpCTA/index.jsx
+++ client/src/components/SignUpCTA/index.jsx
@@ -3,12 +3,16 @@ import React from 'react';
 // Styled components
 import { Container, Info } from './SignUpCTA.styles';
 
+import { Link } from 'react-router-dom';
+
 const SignUpCTA = () => {
   return (
     <Container>
       <Info>
         <h3>Won't you join us on this journey? Sign up now!</h3>
-        <button>Sign me up</button>
+        <Link to="/sign-up" style={{ color: '#fff' }}>
+          <button>Sign me up</button>
+        </Link>
         <p>No credit card needed.</p>
       </Info>
     </Container>
