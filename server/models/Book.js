80b739d1f447af5f43d4461aed2bb71291f2fdb6
andrewphm <andrpham@gmail.com>
2021-12-30 15:37:49 -0500

    Update book schema

--- server/models/Book.js
+++ server/models/Book.js
@@ -3,11 +3,11 @@ const mongoose = require('mongoose');
 const BookSchema = new mongoose.Schema(
   {
     book: { type: String, required: true },
-    author: { type: Object, required: true },
+    author: { type: String, required: true },
     price: { type: Number },
     category: { type: Array },
     details: { type: Object },
-    reviews: { type: Object },
+    reviews: { type: Array },
     description: { type: String },
     images: { type: Object },
   },
