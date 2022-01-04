4c53cee15da3b68dade9dbf677a2848dc02f615b
andrewphm <andrpham@gmail.com>
2022-01-04 17:18:16 -0500

    Add responsive styling

--- client/src/components/LatestNews/LatestNews.styles.js
+++ client/src/components/LatestNews/LatestNews.styles.js
@@ -8,6 +8,7 @@ export const Card = styled.div`
   box-shadow: var(--box-shadow);
   transition: all 0.35s ease-in-out;
   background-color: white;
+  margin: 0 auto;
 
   :hover {
     transform: scale(1.03);
@@ -21,11 +22,7 @@ export const Card = styled.div`
   }
 `;
 
-export const Anchor = styled.a`
-  :not(:first-child) {
-    display: none;
-  }
-`;
+export const Anchor = styled.a``;
 
 export const CardInfo = styled.div`
   display: flex;
@@ -48,7 +45,6 @@ export const CardInfo = styled.div`
     width: fit-content;
     cursor: pointer;
     font-weight: 600;
-    transition: all 0.35s ease;
 
     :hover {
       transform: scale(1.05);
@@ -60,7 +56,6 @@ export const Section = styled.section`
   background-image: url(${(props) => props.backgroundImg});
   background-repeat: no-repeat;
   background-size: 200%;
-  height: 500px;
   padding: 20px;
 `;
 
@@ -69,18 +64,21 @@ export const Container = styled.div`
   flex-direction: column;
   justify-content: space-between;
   height: 100%;
+  width: 100%;
+  margin: 1rem 0;
 
   h2 {
+    font-size: clamp(20px, 4vw, 26px);
     color: white;
   }
 `;
 
 export const NewsContainer = styled.div`
   display: grid;
-  width: auto;
+  width: 100%;
   gap: 2rem;
-  grid-template-columns: 1fr;
-  margin: 2rem auto;
+  grid-template-columns: repeat(auto-fill, minmax(340px, 1fr));
+  margin: 1rem auto;
 `;
 
 export const Button = styled.button`
@@ -88,11 +86,12 @@ export const Button = styled.button`
   border: 1px solid var(--primary-color);
   padding: 8px 15px;
   border-radius: 18px;
-  background-color: transparent;
+  background-color: white;
   font-weight: 600;
   width: 200px;
   font-size: 1.1rem;
   margin: 0 auto;
+  margin-top: 2.5rem;
   transition: all 0.35s ease;
   cursor: pointer;
   font-family: var(--primary-font);
@@ -100,5 +99,6 @@ export const Button = styled.button`
   :hover {
     color: white;
     background-color: var(--primary-color);
+    border: 1px solid white;
   }
 `;
