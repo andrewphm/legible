bed58129c8c85d9c32ea0f774ea261f19658023b
andrewphm <andrpham@gmail.com>
2022-01-04 13:10:01 -0500

    Add styles to productcover component

--- client/src/components/ProductCover/ProductCover.styles.js
+++ client/src/components/ProductCover/ProductCover.styles.js
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 export const Container = styled.div`
   width: 135px;
   height: 294px;
+  display: flex;
+  flex-direction: column;
 `;
 
 export const ImgWrapper = styled.div`
@@ -13,11 +15,27 @@ export const Thumb = styled.img`
   width: 100%;
 `;
 
-export const Title = styled.h3``;
+export const Title = styled.h3`
+  width: 100%;
+  font-size: 14px;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  white-space: nowrap;
+`;
 
-export const Author = styled.p``;
+export const Author = styled.p`
+  font-size: 14px;
+  width: 100%;
+  font-size: 14px;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  white-space: nowrap;
+`;
 
-export const Price = styled.p``;
+export const Price = styled.p`
+  margin: 0.4rem 0;
+  font-weight: 500;
+`;
 
 export const LinkContainer = styled.div`
   display: flex;
@@ -25,11 +43,12 @@ export const LinkContainer = styled.div`
   .heart-border {
     cursor: pointer;
 
-    :hover {
+    :hover path[d] {
+      transform: scale(1.1);
       fill: rgb(203, 14, 14);
       d: path(
         'M 12 21.35 l -1.45 -1.32 C 5.4 15.36 2 12.28 2 8.5 C 2 5.42 4.42 3 7.5 3 c 1.74 0 3.41 0.81 4.5 2.09 C 13.09 3.81 14.76 3 16.5 3 C 19.58 3 22 5.42 22 8.5 c 0 3.78 -3.4 6.86 -8.55 11.54 L 12 21.35 Z'
-      );
+      ) !important;
     }
   }
 `;
--- client/src/components/ProductCover/index.jsx
+++ client/src/components/ProductCover/index.jsx
@@ -18,12 +18,15 @@ const ProductCover = ({ book }) => {
       <ImgWrapper>
         <Thumb src="https://imgcdn2.legible.com/legible/w:342/plain/s3://legibledepot/855b41f4-81f2-47b0-9fd1-b703fd9c8f25/metadata/20211007/9781459747104.jpg"></Thumb>
       </ImgWrapper>
-      <Title>Homeland Elegies</Title>
-      <Author>by Ayad Akhtar</Author>
+      <div>
+        <Title>Homeland Elegies Homeland Elegies</Title>
+      </div>
+      <div>
+        <Author>by Ayad Akhtar</Author>
+      </div>
       <Price>CAD $14.99</Price>
       <LinkContainer>
         <FavoriteBorderOutlined className="heart-border"></FavoriteBorderOutlined>
-        <Favorite></Favorite>
       </LinkContainer>
     </Container>
   );
