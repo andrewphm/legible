f9c49bd2e0327bd5baf316c35f3d0888e217e2fd
andrewphm <andrpham@gmail.com>
2022-01-06 03:48:38 -0500

    Add trycatch block to catch rejects on stripe payment intent

--- client/src/pages/Purchase/Purchase.styles.js
+++ client/src/pages/Purchase/Purchase.styles.js
@@ -1,5 +1,12 @@
 import styled from 'styled-components';
 
-export const Main = styled.main``;
+export const Main = styled.main`
+  padding: 0px;
+`;
 
-export const Section = styled.section``;
+export const Section = styled.section`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  padding: 5rem 0;
+`;
--- server/controllers/stripe.js
+++ server/controllers/stripe.js
@@ -14,17 +14,24 @@ const createPaymentIntent = async (req, res) => {
   // Create a PaymentIntent with the order amount and currency
 
   let price = await calculateOrderAmount(items);
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount: price,
-    currency: 'cad',
-    automatic_payment_methods: {
-      enabled: true,
-    },
-  });
 
-  res.status(200).json({
-    clientSecret: paymentIntent.client_secret,
-  });
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: price,
+      currency: 'cad',
+      automatic_payment_methods: {
+        enabled: true,
+      },
+    });
+    res.status(200).json({
+      clientSecret: paymentIntent.client_secret,
+    });
+  } catch (error) {
+    res.status(400).json({
+      Success: false,
+      Message: 'Could not create Payment Intent',
+    });
+  }
 };
 
 module.exports = { createPaymentIntent };
