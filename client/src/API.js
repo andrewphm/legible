cbecfc1914ac864d67e0ea691a96570a37d15984
andrewphm <andrpham@gmail.com>
2022-01-12 15:11:06 -0500

    add server link

--- client/src/API.js
+++ client/src/API.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:5000/api/';
+const BASE_URL = 'https://legible-server.herokuapp.com/api/';
 
 const apiSettings = {
   checkUniqueUser: async (user) => {
